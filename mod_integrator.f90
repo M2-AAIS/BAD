c3a7a452cae93b5e5a9c451ddcf0743155905429
Corentin Cadiou <contact@cphyc.me>
2015-10-16 18:26:53 +0200

    FIx collapse of type name and variable name

--- mod_integrator.f90
+++ mod_integrator.f90
@@ -8,12 +8,12 @@ module mod_integrator
   
 contains
   
-  subroutine do_timestep (state)
+  subroutine do_timestep (states)
     use mod_constants
     use mod_timestep
     implicit none
     
-    type (state), intent(in), dimension(n_cell)   :: state
+    type (state), intent(in), dimension(n_cell)   :: states
 
     real(kind = x_precision)                      :: dt, dx2, dx = 0
     integer                                       :: i, info
@@ -22,21 +22,21 @@ contains
     real(kind = x_precision), dimension(n_cell-1) :: diag_low, diag_up
     
     ! Get the timestep
-    call timestep(state, dt)
+    call timestep(states, dt)
 
     dx2 = dx**2
     
     ! Create the diagonals and copy T and S
-    diag     = 1/dt + 2/dx2 * state(:)%nu / state(:)%x
-    diag_up  = -1/dx2 * state(2:n_cell)%nu / state(:)%x**2
-    diag_low = -1/dx2 * state(1:n_cell-1)%nu / state(:)%x**2
+    diag     = 1/dt + 2/dx2 * states(:)%nu / states(:)%x
+    diag_up  = -1/dx2 * states(2:n_cell)%nu / states(1:n_cell-1)%x**2
+    diag_low = -1/dx2 * states(1:n_cell-1)%nu / states(2:n_cell)%x**2
 
-    diag_up(1)           = -1/dx2 * state(2)%nu / state(1)%x**2
-    diag(n_cell)         = state(n_cell)%nu
-    diag_low(n_cell - 1) = state(n_cell-1)%nu
+    diag_up(1)           = -1/dx2 * states(2)%nu / states(1)%x**2
+    diag(n_cell)         = states(n_cell)%nu
+    diag_low(n_cell - 1) = states(n_cell-1)%nu
     
     ! Solving for S
-    call dgtsv(n_cell, 1, diag_low, diag, diag_up, state_out(:)%S, n_cell, info)
+    call dgtsv(n_cell, 1, diag_low, diag, diag_up, states(:)%S, n_cell, info)
     
     if (info /= 0) then
        print *, "Ooops, something bad happened!"
