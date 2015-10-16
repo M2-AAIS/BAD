85cc514209320d664e4768c9452d96be28d2229c
Corentin Cadiou <contact@cphyc.me>
2015-10-16 16:14:29 +0200

    Placeholder for integration

--- mod_integrator.f90
+++ mod_integrator.f90
@@ -1,4 +1,4 @@
-! Module that exposes the integrator that transforms the state_in to thet state_out
+! Module that exposes the integrator that transforms the state_in to the state_out
 
 module mod_integrator
   
@@ -10,9 +10,24 @@ contains
   
   subroutine do_timestep (state_in, state_out)
     use mod_constants
+    use mod_timestep
     implicit none
+    
     type (state), intent(in), dimension(:)  :: state_in
     type (state), intent(out), dimension(:) :: state_out
+
+    real(kind = x_precision)                :: timestep
+    integer                                 :: i
+
+    ! Get the timestep
+    call timestep(state_in, timestep)
+    
+    ! Do the integration
+    do i = 0, n_iterations
+       ! Integrate equation 1
+       ! Integrate equation 2
+       ! Integrate equation 3
+    end do
     
   end subroutine do_timestep
   
--- mod_timestep.f90
+++ mod_timestep.f90
@@ -8,10 +8,13 @@ module mod_timestep
   
 contains
   
-  subroutine timestep (state_in)
+  subroutine timestep (state_in, timestep)
     use mod_constants
     implicit none
     type (state), intent(in), dimension(:)  :: state_in
+    real(kind = x_precision)                :: timestep
+
+    
     
   end subroutine timestep
   
