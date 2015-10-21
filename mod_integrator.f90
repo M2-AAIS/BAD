c3cf8ef6b135cb23e9c4d3dfaf4c8159b2d86d3e
Corentin Cadiou <contact@cphyc.me>
2015-10-21 12:08:52 +0200

    Use numerical approx for T integration

--- mod_integrator.f90
+++ mod_integrator.f90
@@ -11,21 +11,57 @@ module mod_integrator
   public :: do_timestep
   
 contains
+
+  function f (s, dt, dx)
+    use mod_constants
+    use mod_variables
+    implicit none
+    
+    type(state), intent(in)                    :: s
+    real (kind=x_precision), intent(in)        :: dt, dx
+    real (kind=x_precision), dimension(n_cell) :: f
+
+    real (kind=x_precision)                    :: overdx, overdt
+    real (kind=x_precision), dimension(n_cell) :: dS_over_dt, dS_over_x_over_dx, S_over_x, dT_over_dx
+
+    overdx = 1/dx
+    overdt = 1/dt
+
+    dT_over_dx(1:n_cell - 1) = (s%T(2:n_cell) - s%T(1:n_cell - 1)) * overdx
+    dT_over_dx(n_cell)       = 0
+
+    S_over_x                        = s%S / s%x
+    dS_over_x_over_dx(1:n_cell - 1) = (S_over_x(2:n_cell) - S_over_x(1:n_cell-1)) * overdx
+    dS_over_x_over_dx(n_cell)       = 0 !TODO
+
+    dS_over_dt  = 0 !TODO
+
+    dS_over_x_over_dx(1:n_cell - 1) = (S_over_x(2:n_cell) - S_over_x(1:n_cell-1)) * overdx
+    dS_over_dt = 0
+    
+    f = 3_x_precision*state_0%v_0**2/state_0%T_0 * s%nu * s%Omega**2 - &
+         s%Fz*s%x/s%S &
+         + kmp/mu * (4._x_precision-3._x_precision*s%beta)/s%beta * s%T/s%S * &
+         (dS_over_dt + s%v*dS_over_x_over_dx) &
+         - s%cv*s%v/s%x * dT_over_dx
+  end function f
+
   
   subroutine do_timestep (states, param)
     use mod_variables
     use mod_constants
     implicit none
-
+    
     type (state), intent(inout)                   :: states
     type (parameters), intent(in)                 :: param
 
-    real(kind = x_precision)                      :: dt, overdx, overdx2, overdt, dx, B
-    integer                                       :: info, i
+    type (state)                                  :: states_deriv
+    real(kind = x_precision)                      :: dt, overdx, overdx2, overdt, dx
+    integer                                       :: info
 
-    real(kind = x_precision), dimension(n_cell)   :: diag, dT_over_dx, S_over_x, dS_over_x_over_dx
-    real(kind = x_precision), dimension(n_cell)   :: dS_over_dt, dH_over_dT, f0, f1, dFz_over_dT, prevS
+    real(kind = x_precision), dimension(n_cell)   :: dtemp, T_deriv, diag
     real(kind = x_precision), dimension(n_cell-1) :: diag_low, diag_up
+    real(kind = x_precision), dimension(n_cell)   :: f0, f1
     
     ! Get the timestep and the spacestep
     call timestep(states, dt)
@@ -45,9 +81,6 @@ contains
     diag_up(1)         = 0
     diag(n_cell)       = states%nu(n_cell)
     diag_low(n_cell-1) = states%nu(n_cell-1)
-
-    ! Store previous S
-    prevS = states%S
     
     ! Solving for S
     call dgtsv(n_cell, 1, diag_low, diag, diag_up, states%S, n_cell, info)
@@ -55,32 +88,14 @@ contains
     if (info /= 0) then
        print *, "Ooops, something bad happened!"
     end if
-
-    ! Compute useful quantities
-    dT_over_dx(1:n_cell - 1) = (states%T(2:n_cell) - states%T(1:n_cell - 1)) * overdx
-    dT_over_dx(n_cell)       = 0
-
-    S_over_x                        = states%S / states%x
-    dS_over_x_over_dx(1:n_cell - 1) = (S_over_x(2:n_cell) - S_over_x(1:n_cell-1)) * overdx
-    dS_over_x_over_dx(n_cell)       = 0 !TODO
-
-    dS_over_dt  = 0 !TODO
-    dH_over_dT  = 0 !TODO
-    dFz_over_dT = 0 !TODO
-
-    B = state_0%P_rad_0 / state_0%P_gaz_0
     
-    f0 = 3_x_precision*state_0%v_0**2/state_0%T_0 * states%nu * states%Omega**2 - &
-         states%Fz*states%x/states%S &
-         + kmp/mu * (4._x_precision-3._x_precision*states%beta)/states%beta * states%T/states%S * &
-           ((states%S - prevS)/dt + states%v*dS_over_x_over_dx) &
-         - states%Cv*states%v/states%x * dT_over_dx
+    call compute_variables(states%x, states%Omega, T_deriv, states%S, states_deriv)
+    f0 = f(states, dt, dx)
+    dtemp = 0.01_x_precision * states%T
+    T_deriv = states%T + dtemp
 
-    f1 = 3._x_precision*state_0%v_0**2/state_0%T_0 * (2*param%alpha * states%H * dH_over_dT * states%Omega**3) + &
-         dFz_over_dT * states%x / states%S + &
-         kmp/mu * (1._x_precision - 12._x_precision*B*states%T**3 / states%rho) * (dS_over_dt + states%v * dS_over_x_over_dx )
-    !              \___________ (4-3beta)/beta * T*/S* ________________________/
-
+    f1 = (f(states_deriv, dt, dx) - f(states, dt, dx)) / dtemp
+    
     ! Solve for T
     states%T = states%T + f0 / (1_x_precision / dt - f1)
 
