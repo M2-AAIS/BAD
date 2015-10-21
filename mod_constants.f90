38bb28729765481596b1b699ecc88e7f5c94d439
Bruno Pagani <bruno.n.pagani@gmail.com>
2015-10-21 15:47:34 +0200

    Lint the code

--- mod_constants.f90
+++ mod_constants.f90
@@ -7,47 +7,47 @@ module mod_constants
   integer, parameter, public :: n_cell = 256
   integer, parameter, public :: n_iterations = 1000
 
-  real(kind = x_precision), parameter, public :: G = 6.67408e-8_x_precision !gravitationnal cst in cgs
-  real(kind = x_precision), parameter, public :: c = 2.99792458e10_x_precision !speed of light in cgs
-  real(kind = x_precision), parameter, public :: pi = 4.0_x_precision*atan(1.0_x_precision) 
-  real(kind = x_precision), parameter, public :: M_sol = 1.98855e33_x_precision !mass of the sun in cgs
+  real(kind = x_precision), parameter, public :: G       = 6.67408e-8_x_precision !gravitationnal cst in cgs
+  real(kind = x_precision), parameter, public :: c       = 2.99792458e10_x_precision !speed of light in cgs
+  real(kind = x_precision), parameter, public :: pi      = 4.0_x_precision*atan(1.0_x_precision) 
+  real(kind = x_precision), parameter, public :: M_sol   = 1.98855e33_x_precision !mass of the sun in cgs
   real(kind = x_precision), parameter, public :: cst_rad = 7.5657308531642009e-17_x_precision !constant of radiation in cgs
-  real(kind = x_precision), parameter, public :: stefan = (c * cst_rad) / 4.0_x_precision !stefan cst in cgs
-  real(kind = x_precision), parameter, public :: kmp = 8.3144598_x_precision !ratio of the boltzmann cst over the proton massin cgs
-  real(kind = x_precision), parameter, public :: mu = 0.62_x_precision
-  real(kind = x_precision), parameter, public :: gammag = 5._x_precision / 3._x_precision 
+  real(kind = x_precision), parameter, public :: stefan  = (c * cst_rad) / 4.0_x_precision !stefan cst in cgs
+  real(kind = x_precision), parameter, public :: kmp     = 8.3144598_x_precision !boltzmann cst over proton mass in cgs = gas cst
+  real(kind = x_precision), parameter, public :: mu      = 0.62_x_precision
+  real(kind = x_precision), parameter, public :: gammag  = 5._x_precision / 3._x_precision 
 
   type parameters
-     real(kind = x_precision) :: M, Mdot, X, mu, alpha, rmax
-     !M     : Black hole Mass
-     !Mdot  : Acretion rate at rmax
-     !X     : Chemical composition
-     !mu    : Atomic mass
-     !alpha : Viscosity parameter
-     !rmax  : Maximal radius of accretion disk
+    real(kind = x_precision) :: M, Mdot, X, mu, alpha, rmax
+    !M     : Black hole Mass
+    !Mdot  : Acretion rate at rmax
+    !X     : Chemical composition
+    !mu    : Atomic mass
+    !alpha : Viscosity parameter
+    !rmax  : Maximal radius of accretion disk
   end type parameters
 
   type state
-     real(kind = x_precision), dimension(n_cell) :: Omega, x, nu, v, T, P_rad, P_gaz, beta, cs, H, rho, S, Fz, M_dot, Cv
-     !Omega : Angular velocity
-     !x     : Space variable
-     !nu    : Viscosity
-     !v     : Local speed accretion
-     !T     : Temperature
-     !P_rad : Radiative pressure
-     !P_gaz : Gaz pressure
-     !beta  : Pressure indicator
-     !cs    : Speed of sound
-     !H     : Half height of disk
-     !rho   : Volume density
-     !S     : Variable of density
-     !Fz    : Radiative Flux
-     !M_dot : acretion rate
-     !s     : S
+    real(kind = x_precision), dimension(n_cell) :: Omega, x, nu, v, T, P_rad, P_gaz, beta, cs, H, rho, S, Fz, M_dot, Cv
+    !Omega : Angular velocity
+    !x     : Space variable
+    !nu    : Viscosity
+    !v     : Local, radial accretion speed
+    !T     : Temperature
+    !P_rad : Radiative pressure
+    !P_gaz : Gaz pressure
+    !beta  : Pressure indicator
+    !cs    : Speed of sound
+    !H     : Disk half-height
+    !rho   : Volume density
+    !S     : Variable of density
+    !Fz    : Radiative flux
+    !M_dot : Accretion rate
+    !Cv    : Heat capacity at constant volume
   end type state
 
   type adim_state
-     real(kind= x_precision) :: Omega_0, temps_0, x_0, nu_0, v_0, cs_0, S_0, H_0, M_dot_0, rho_0, T_0, Fz_0, Cv_0, P_gaz_0, P_rad_0
+    real(kind= x_precision) :: Omega_0, temps_0, x_0, nu_0, v_0, cs_0, S_0, H_0, M_dot_0, rho_0, T_0, Fz_0, Cv_0, P_gaz_0, P_rad_0
   end type adim_state
   
 contains
--- mod_variables.f90
+++ mod_variables.f90
@@ -35,41 +35,41 @@ contains
     Delta = b1**2 - (4._x_precision * a1 * c1)
     !--------------------------------------------------------
     do i=1,n_cell
-       state_out%H(i)     = - 0.5_x_precision * (b1(i) + sign(sqrt(Delta(i)),b1(i))) / a1(i) 
-       state_out%P_rad(i) = T_in(i)**4
-       state_out%cs(i)    = Omega(i) * state_out%H(i)
-       state_out%rho(i)   = S_in(i) / (state_out%H(i) * x(i))
-       state_out%nu(i)    = para%alpha * state_out%cs(i) * state_out%H(i)
-       state_out%P_gaz(i) = state_out%rho(i) * (T_in(i)**4)
-       state_out%beta(i)  = state_out%P_gaz(i) / (state_out%P_gaz(i) + state_out%P_rad(i))
-       state_out%Cv(i)    = (kmp/mu) * ((12._x_precision * gammag -1._x_precision ) &
-            * (1._x_precision - state_out%beta(i)) + state_out%beta(i)) &
-            / ( state_out%beta(i) * (gammag - 1._x_precision))
-       !------------limit condition to compute v-------------
-       if (i .eq. 1) then
-          state_out%v(i)  = 0._x_precision
-       else if (i .eq. n_cell) then
-          state_out%v(i)  = - 1._x_precision / S_in(i) / x(i) 
-       else
-          state_out%v(i)  = - 1._x_precision / S_in(i) / x(i) * ( ((state_out%nu(i+1) &
-               * S_in(i+1)) - (state_out%nu(i-1) * S_in(i-1))) / (2._x_precision * (x(i+1) - x(i-1))))
-       endif
-       state_out%M_dot(i) = - state_out%v(i) * S_in(i) * x(i)
+      state_out%H(i)     = - 0.5_x_precision * (b1(i) + sign(sqrt(Delta(i)),b1(i))) / a1(i) 
+      state_out%P_rad(i) = T_in(i)**4
+      state_out%cs(i)    = Omega(i) * state_out%H(i)
+      state_out%rho(i)   = S_in(i) / (state_out%H(i) * x(i))
+      state_out%nu(i)    = para%alpha * state_out%cs(i) * state_out%H(i)
+      state_out%P_gaz(i) = state_out%rho(i) * (T_in(i)**4)
+      state_out%beta(i)  = state_out%P_gaz(i) / (state_out%P_gaz(i) + state_out%P_rad(i))
+      state_out%Cv(i)    = (kmp/mu) * ((12._x_precision * gammag -1._x_precision ) &
+                           * (1._x_precision - state_out%beta(i)) + state_out%beta(i)) &
+                           / ( state_out%beta(i) * (gammag - 1._x_precision))
+      !------------limit condition to compute v-------------
+      if (i .eq. 1) then
+        state_out%v(i)  = 0._x_precision
+      else if (i .eq. n_cell) then
+        state_out%v(i)  = - 1._x_precision / S_in(i) / x(i) 
+      else
+        state_out%v(i)  = - 1._x_precision / S_in(i) / x(i) * ( ((state_out%nu(i+1) &
+                          * S_in(i+1)) - (state_out%nu(i-1) * S_in(i-1))) / (2._x_precision * (x(i+1) - x(i-1))))
+      endif
+      state_out%M_dot(i) = - state_out%v(i) * S_in(i) * x(i)
       !--------------Compute varaibles for Fz---------------
-       kappa_ff(i) = 6.13e22_x_precision * state_0%rho_0 * state_out%rho(i) * &
-            (state_0%T_0 * T_in(i))**(-7._x_precision/2._x_precision) 
-       tau(i)      = 0.5_x_precision * sqrt(0.2_x_precision * (1._x_precision * para%X) * 6.13e22 * state_0%rho_0 &
-            * state_out%rho(i) * (state_0%T_0 * T_in(i))**(-7._x_precision/2._x_precision)) * state_0%S_0 &
-            * S_in(i) / x(i)
-       epsil(i)    = 6.22e20_x_precision * (state_0%rho_0 * state_out%rho(i))**2  &
-            * sqrt((state_0%T_0 * T_in(i))) 
+      kappa_ff(i) = 6.13e22_x_precision * state_0%rho_0 * state_out%rho(i) * &
+                    (state_0%T_0 * T_in(i))**(-7._x_precision/2._x_precision) 
+      tau(i)      = 0.5_x_precision * sqrt(0.2_x_precision * (1._x_precision * para%X) * 6.13e22_x_precision * state_0%rho_0 &
+                    * state_out%rho(i) * (state_0%T_0 * T_in(i))**(-7._x_precision/2._x_precision)) * state_0%S_0 &
+                    * S_in(i) / x(i)
+      epsil(i)    = 6.22e20_x_precision * (state_0%rho_0 * state_out%rho(i))**2  &
+                    * sqrt((state_0%T_0 * T_in(i))) 
 
-       if (tau(i) .ge. 1.0) then
-          state_out%Fz(i) = (4._x_precision * c * c * T_in(i)**4) / (27._x_precision * sqrt(3.0) * &
-               (kappa_ff(i) + kappa_e) * (S_in(i)/x(i) * state_0%S_0)) 
-       else
-          state_out%Fz(i) = epsil(i) * state_0%H_0 * state_out%H(i)
-       endif
+      if (tau(i) .ge. 1.0) then
+        state_out%Fz(i) = (4._x_precision * c * c * T_in(i)**4) / (27._x_precision * sqrt(3.0) * &
+                          (kappa_ff(i) + kappa_e) * (S_in(i)/x(i) * state_0%S_0)) 
+      else
+        state_out%Fz(i) = epsil(i) * state_0%H_0 * state_out%H(i)
+      endif
     enddo
   end subroutine compute_variables
   !--------------------------------------------------------------
@@ -80,32 +80,32 @@ contains
     !select 0 or 1 to adimension or dimension your state 
     integer         , intent(in)                 :: mode
     type(state)     , intent(inout)              :: state_in
-	
+
     select case(mode)
     case(0)
-       state_in%H     = state_in%H / state_0%H_0
-       state_in%v     = state_in%v / state_0%v_0
-       state_in%cs    = state_in%cs / state_0%cs_0
-       state_in%S     = state_in%S / state_0%S_0
-       state_in%T     = state_in%T / state_0%T_0
-       state_in%rho   = state_in%rho / state_0%rho_0
-       state_in%nu    = state_in%nu / state_0%nu_0
-       state_in%M_dot = state_in%M_dot / state_0%M_dot_0
-       state_in%P_rad = state_in%P_rad / state_0%P_rad_0
-       state_in%P_gaz = state_in%P_gaz / state_0%P_gaz_0
-       state_in%Fz    = state_in%Fz / state_0%Fz_0
+      state_in%H     = state_in%H     / state_0%H_0
+      state_in%v     = state_in%v     / state_0%v_0
+      state_in%cs    = state_in%cs    / state_0%cs_0
+      state_in%S     = state_in%S     / state_0%S_0
+      state_in%T     = state_in%T     / state_0%T_0
+      state_in%rho   = state_in%rho   / state_0%rho_0
+      state_in%nu    = state_in%nu    / state_0%nu_0
+      state_in%M_dot = state_in%M_dot / state_0%M_dot_0
+      state_in%P_rad = state_in%P_rad / state_0%P_rad_0
+      state_in%P_gaz = state_in%P_gaz / state_0%P_gaz_0
+      state_in%Fz    = state_in%Fz    / state_0%Fz_0
     case(1)
-       state_in%H     = state_in%H * state_0%H_0
-       state_in%v     = state_in%v * state_0%v_0
-       state_in%cs    = state_in%cs * state_0%cs_0
-       state_in%S     = state_in%S * state_0%S_0
-       state_in%T     = state_in%T * state_0%T_0
-       state_in%rho   = state_in%rho * state_0%rho_0
-       state_in%nu    = state_in%nu * state_0%nu_0
-       state_in%M_dot = state_in%M_dot * state_0%M_dot_0
-       state_in%P_rad = state_in%P_rad * state_0%P_rad_0
-       state_in%P_gaz = state_in%P_gaz * state_0%P_gaz_0
-       state_in%Fz    = state_in%Fz * state_0%Fz_0
+      state_in%H     = state_in%H     * state_0%H_0
+      state_in%v     = state_in%v     * state_0%v_0
+      state_in%cs    = state_in%cs    * state_0%cs_0
+      state_in%S     = state_in%S     * state_0%S_0
+      state_in%T     = state_in%T     * state_0%T_0
+      state_in%rho   = state_in%rho   * state_0%rho_0
+      state_in%nu    = state_in%nu    * state_0%nu_0
+      state_in%M_dot = state_in%M_dot * state_0%M_dot_0
+      state_in%P_rad = state_in%P_rad * state_0%P_rad_0
+      state_in%P_gaz = state_in%P_gaz * state_0%P_gaz_0
+      state_in%Fz    = state_in%Fz    * state_0%Fz_0
     case default
        stop
     end select
@@ -134,7 +134,7 @@ contains
     state_0%M_dot_0 = para%Mdot
     state_0%rho_0   = state_0%S_0 / (2._x_precision*rs)
     state_0%T_0     = (1._x_precision/sqrt(27.0) * 1._x_precision/48._x_precision * para%Mdot * c2 &
-         / ( pi * rs * rs * stefan ))**0.25_x_precision
+                      / ( pi * rs * rs * stefan ))**0.25_x_precision
     state_0%Fz_0    = state_0%S_0 / 2._x_precision / state_0%temps_0
     state_0%Cv_0    = 1._x_precision / state_0%T_0
     state_0%P_gaz_0 = state_0%rho_0 * kmp * state_0%T_0 / mu
