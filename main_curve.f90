c5581bb046d1ad653472f6f69063b4e962019a1b
Aristera <mich080@hotmail.com>
2015-11-20 18:31:46 +0100

    Update main_curve.f90
    ,

--- main_curve.f90
+++ main_curve.f90
@@ -8,6 +8,26 @@ program black_hole_s_curve
 
   call get_parameters() 
   call init_variable_0()
-  call curve()
+    integer                                                 :: i
+
+    real(kind = x_precision),dimension(n_cell)              :: temperature
+    real(kind = x_precision),dimension(n_cell)              :: s
+
+
+  call get_parameters() 
+  call init_variable_0()
+  
+     call curve( temperature,s)
+
+  do i          = 1, n_cell
+
+    
+     write(*,*)'**********************************'
+     write(*,*)'T = ',temperature(i)
+     write(*,*)'S = ',s(i)
+     write(*,*)'**********************************'
+
+  enddo
+
   
 end program black_hole_s_curve
