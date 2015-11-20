4c65ae554c273ccc10cefec5236295c2f738bccd
Antoine Marchal <antoine.marchal@obspm.fr>
2015-11-20 18:39:20 +0100

    correction main

--- main.f90
+++ main.f90
@@ -18,8 +18,8 @@ program black_hole_diffusion
   ! FIXME
  ! t_V = 1.3e4_x_precision / state_0%temps_0 
  ! t_T = 0.72_x_precision / state_0%temps_0
-  t_V = params%t_nu
-  t_T = params%t_T
+  t_V = params%t_nu / state_0%temps_0
+  t_T = params%t_T / state_0%temps_0
   dt_V = t_V / 100._x_precision
   dt_T = t_T / 100._x_precision
   
