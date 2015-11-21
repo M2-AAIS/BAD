62fc2c023d9514cb0f96cfdaf709658562c08f6f
Clément Hottier <clement.hottier@obspm.fr>
2015-11-21 14:34:04 +0100

    more visual syntax in do_timestep_S_exp

--- mod_integrator.f90
+++ mod_integrator.f90
@@ -9,7 +9,7 @@ module mod_integrator
   implicit none 
   private
 
-  public :: do_timestep_T, do_timestep_S
+  public :: do_timestep_T, do_timestep_S, do_timestep_S_exp
   
 contains
   ! (T(t+dt) - T(t))/dt = f(s, dt)
@@ -90,24 +90,34 @@ contains
   end subroutine do_timestep_S
 
   subroutine do_timestep_S_exp (states, dt)
+    !process the temporal evolution of S with an explicit algortyhm
+    !using only on the superior branch of the S curve
     implicit none
     
-    type (state), intent(inout)                   :: states
-    real (kind = x_precision), intent(in)         :: dt
+    type (state), intent(inout)                   :: states 
+    real (kind = x_precision), intent(in)         :: dt 
     
-    real (kind = x_precision), dimension(n_cell)  :: S_tmp
+    real (kind = x_precision), dimension(n_cell)  :: S_tmp ! temporal array in order to process the state%S at t+dt
 
-    S_tmp(1)          = states%S(1) + (states%nu(2) * states%S(2) - (2._x_precision &
-        * states%nu(1) * states%S(1)) ) /  (params%dx**2 *  x_state%x(1)**2) * dt
+    S_tmp(1)          = states%S(1) + ( & 
+                        states%nu(2) * states%S(2) &
+                        - 2._x_precision  * states%nu(1) * states%S(1) &
+                        ) &
+                        /  (params%dx**2 * x_state%x(1)**2) * dt
     
-    S_tmp(2:n_cell-1) = states%S(2:n_cell-1) + ((states%nu(3:n_cell)  &
-        * states%S(3:n_cell))  - 2._x_precision * (states%nu(2:n_cell-1) & 
-        * states%S(2:n_cell-1)) + ( states%nu(1:n_cell-2) * states%S(1:n_cell-2))) & 
-        / (params%dx**2 *  x_state%x(2:n_cell-1)**2) * dt
+    S_tmp(2:n_cell-1) = states%S(2:n_cell-1) + ( & 
+                        states%nu(3:n_cell) * states%S(3:n_cell) &
+                        - 2._x_precision * states%nu(2:n_cell-1) * states%S(2:n_cell-1) &
+                        +  states%nu(1:n_cell-2) * states%S(1:n_cell-2) &
+                        ) & 
+                        / (params%dx**2 *  x_state%x(2:n_cell-1)**2) * dt
     
-    S_tmp(n_cell)     = states%S(n_cell) + (params%dx - states%nu(n_cell) & 
-        * states%nu(n_cell) + states%nu(n_cell-1) * states%S(n_cell-1)) / & 
-    params%dx**2 * dt
+    S_tmp(n_cell)     = states%S(n_cell) + ( &
+                        params%dx &
+                        - states%nu(n_cell)  * states%nu(n_cell) &
+                        + states%nu(n_cell-1) * states%S(n_cell-1) &
+                        ) & 
+                        / params%dx**2 * dt
     
     states%S = S_tmp
   end subroutine do_timestep_S_exp
