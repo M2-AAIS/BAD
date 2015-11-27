833498f01b43f282a56d57986e0690e0bf87574b
Corentin Cadiou <contact@cphyc.me>
2015-11-27 17:30:35 +0100

    Fully dimension outputs

--- mod_output.f90
+++ mod_output.f90
@@ -27,13 +27,13 @@ contains
 
     write(unit, fmt=*) '# ', iteration
     write(unit, fmt=*) '# ', time
-    write(unit, fmt='(2a, 15(a16))') '# ', 'x', 'Omega', 'nu', 'v', 'T', 'P_rad',&
-         'P_gaz', 'beta', 'cs', 'H', 'rho', 'S', 'Fz', 'M_dot', 'Cv'
+    write(unit, fmt='(2a, 15(a16))') 'r', 'Omega*', 'nu', 'v', 'T', 'P_rad',&
+         'P_gas', 'beta', 'cs', 'H', 'rho', 'S', 'Fz', 'M_dot', 'Cv'
 
     state_dim = s
     call dim_adim(1, state_dim)
     do i = 1, n_cell
-       write(unit, fmt='(15(e16.8e2))') x_state%x(i), x_state%Omega(i), &
+       write(unit, fmt='(15(e16.8e2))') r_state%r(i), x_state%Omega(i), &
             state_dim%nu(i), state_dim%v(i), &
             state_dim%T(i), state_dim%Prad(i), state_dim%Pgaz(i), &
             state_dim%beta(i), state_dim%cs(i), &
