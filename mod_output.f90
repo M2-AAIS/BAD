57d031b0cdc19fe8cb26bc2a7850782889d1e41d
Corentin Cadiou <contact@cphyc.me>
2015-11-27 17:39:22 +0100

    Also output Sigma

--- mod_output.f90
+++ mod_output.f90
@@ -27,17 +27,18 @@ contains
 
     write(unit, fmt=*) '# ', iteration
     write(unit, fmt=*) '# ', time
-    write(unit, fmt='(15(a16))') 'r', 'Omega*', 'nu', 'v', 'T', 'P_rad',&
-         'P_gas', 'beta', 'cs', 'H', 'rho', 'S', 'Fz', 'M_dot', 'Cv'
+    write(unit, fmt='(16(a16))') 'r', 'Omega*', 'nu', 'v', 'T', 'P_rad',&
+         'P_gas', 'beta', 'cs', 'H', 'rho', 'S', 'Sigma', 'Fz', 'M_dot', 'Cv'
 
     state_dim = s
     call dim_adim(1, state_dim)
     do i = 1, n_cell
-       write(unit, fmt='(15(e16.8e2))') r_state%r(i), x_state%Omega(i), &
+       write(unit, fmt='(16(e16.8e2))') r_state%r(i), x_state%Omega(i), &
             state_dim%nu(i), state_dim%v(i), &
             state_dim%T(i), state_dim%Prad(i), state_dim%Pgaz(i), &
             state_dim%beta(i), state_dim%cs(i), &
             state_dim%H(i), state_dim%rho(i), state_dim%S(i), &
+            state_dim%S(i) / x_state%x(i), &
             state_dim%Fz(i), state_dim%Mdot(i), state_dim%Cv(i)
     end do
   end subroutine snapshot
