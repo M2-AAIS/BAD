eba70340de017a93ae20512106043786e402d998
Corentin Cadiou <contact@cphyc.me>
2015-11-28 12:57:46 +0100

    Use animation
    creates two functions:
    - init that plots the initial conditions
    - plotData that plots the data given as argument

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,8 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.animation as animation
+import numpy as np
 
 fig, ((ax11, ax12), (ax21, ax22)) = plt.subplots(2, 2)
 
@@ -64,59 +66,93 @@ class Data:
         for key in self.data:
             yield (key, self.data[key])
 
-        
-def plot(data):
-    ax11.set_xdata(data['x'])
-    ax11.set_ydata(data['T'])
 
-    ax12.set_xdata(data['x'])
-    ax12.set_ydata(data['S'])
+lines = {'r-T': 0,
+         'r-Sigma': 0,
+         'Sigma-T': []}
 
-    # ax21.set_xdata(data['x'])
-    # ax21.set_xdata(data[''])
+def init(ic, crit_pts, s_curves, initial_data):
+    ''' Plot the initial conditions, the critical points and the s_curves'''
+    print(initial_data)
+    ax11.set_xlabel('$r\ (cm)$')
+    ax11.set_ylabel('$T\ (K)$')
+    ax11.plot(ic['r'], ic['T'], '--', label='Initial conditions')
+    ax11.plot(ic['r'], crit_pts['Temp_thin'], '--', label='critical')
 
-    ax22.set_xdata(data['T'])
-    ax22.set_ydata(data['S'])
+    lines['r-T'] = ax11.plot(initial_data['r'], initial_data['T'], label='iteration 0')[0]
+    
+    ax11.set_yscale('log')
+    ax11.grid()
+    ax11.legend()
+
+    ax12.set_xlabel('$r\ (cm)$')
+    ax12.set_ylabel('$\Sigma\ (g.cm^{-2}$')
+    ax12.plot(ic['r'], ic['Sigma'], '--')
+    ax12.plot(ic['r'], crit_pts['Sigma_thin'], '--')
+
+    lines['r-Sigma'] = ax12.plot(initial_data['r'], initial_data['Sigma'])[0]
+    
+    ax12.grid()
+    ax12.set_yscale('log')
+    
+    for ind, s_curve in s_curves:
+        ax22.plot(s_curve['Surface_density'], s_curve['Temperature'],
+                  label='$x_{'+str(ind)+'}$')
+
+    # add the initial data on the s_curve plot
+    lines['Sigma-T'] = [ (ind, ax22.plot(np.log10(initial_data['S'][ind]),
+                                     np.log10(initial_data['T'][ind]), 'o')[0])
+                     for ind, foo in s_curves]
+                             
+
+    ax22.set_xlabel('$\log\Sigma\ (g.cm^{-2})$')
+    ax22.set_ylabel('$\log T\ (K)$')
+    ax22.legend()
+    ax22.grid()
+
+def plotData(args):
+    ''' Update with the new data. Parameters:
+    args: array that contains
+      index: the indexes of the datadump
+      data : the data
+    '''
+
+    index, data = args
+    lines['r-T'].set_ydata(data['T'])
+    lines['r-T'].set_label('iteration {}'.format(index))
+
+    lines['r-Sigma'].set_ydata(data['Sigma'])
+
+    for ind, line in lines['Sigma-T']:
+        line.set_xdata(np.log10(data['S'][ind]))
+        line.set_ydata(np.log10(data['T'][ind]))
+
+    ax11.legend()
+    
 
 if __name__ == '__main__':
     print('Reading initial conditions')
     ic = pd.read_csv('CI.dat', delim_whitespace=True)
     
     print('Reading output file')
-    data = Data('output.dat')
+    simulData = Data('output.dat')
 
     print('Reading critical points')
     crit_pts = pd.read_csv('critical_points/file.dat', delim_whitespace=True)
 
     print('Reading S_curves')
-    s_curves = [ pd.read_csv('s_curves/Temperature_Sigma_00001_tot.dat', delim_whitespace=True, dtype=float, header=0),
-                 pd.read_csv('s_curves/Temperature_Sigma_00010_tot.dat', delim_whitespace=True, dtype=float, header=0),
-                 pd.read_csv('s_curves/Temperature_Sigma_00100_tot.dat', delim_whitespace=True, dtype=float)]
+    s_curves_indexes = [1, 10, 100]
+    s_curves = [ (ind, pd.read_csv('s_curves/Temperature_Sigma_{:0>5}_tot.dat'.format(ind),
+                                   delim_whitespace=True, dtype=float, header=0))
+                 for ind in s_curves_indexes]
     
 
-    x0 = data.data[0]
+    initFun = lambda: init(ic, crit_pts, s_curves, simulData.data[0])
 
-    ax11.set_xlabel('$r$')
-    ax11.set_ylabel('$T$')
-    ax11.plot(ic['r'], ic['T'], label='Initial conditions')
-    ax11.plot(x0['r'], x0['T'], label='After 0 iteration')
-    ax11.plot(x0['r'], crit_pts['Temp_thin'])
-    ax11.plot(label='Critical points')
-    ax11.set_yscale('log')
-    ax11.legend()
+    if len(simulData.data) > 1:
+        ani = animation.FuncAnimation(fig, plotData, simulData, init_func=initFun, interval=1)
+    else:
+        initFun()
+        plotData((0, simulData.data[0]))
 
-    ax12.set_xlabel('$r$')
-    ax12.set_ylabel('$\Sigma$')
-    ax12.plot(ic['r'], ic['Sigma'])
-    ax12.plot(x0['r'], x0['Sigma'])
-    ax12.plot(x0['r'], crit_pts['Sigma_thin'])
-    ax12.set_yscale('log')
-
-    ax22.set_xlabel('$\log\Sigma$')
-    ax22.set_ylabel('$\log T$')
-    
-    # ax22.plot(ic['Sigma'], ic['T'])
-    # ax22.plot(x0['Sigma'], x0['T'])
-    for s_curve in s_curves:
-        ax22.plot(s_curve['Surface_density'], s_curve['Temperature'])
     plt.show()
