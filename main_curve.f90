5c761df688313dc116555a58906c5fbde33e8764
Bruno Pagani <bruno.n.pagani@gmail.com>
2015-12-07 01:03:00 +0100

    main_curve: Update Tmin to get cleaner curves

--- main_curve.f90
+++ main_curve.f90
@@ -7,7 +7,7 @@ program black_hole_s_curve
 
   ! Parameters for s_curve
   real(kind = x_precision), parameter         :: eps_in = 1.e-8_x_precision  ! Precision required for the dichotomy
-  real(kind = x_precision), parameter         :: Tmin   = 2.e-1_x_precision
+  real(kind = x_precision), parameter         :: Tmin   = 1.e-1_x_precision
   real(kind = x_precision), parameter         :: Tmax   = 4.49e0_x_precision
   real(kind = x_precision), parameter         :: Smin   = 2.36e2_x_precision
   real(kind = x_precision), parameter         :: Smax   = 2.36e3_x_precision
