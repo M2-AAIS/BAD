e2f67456b06b861f977bea8c6f0a57665749a645
Bruno Pagani <bruno.n.pagani@gmail.com>
2015-12-09 00:25:19 +0100

    mod_constants: Change parameters to actual best
    Also a cosmetic change in main.

--- main.f90
+++ main.f90
@@ -11,7 +11,7 @@ program black_hole_diffusion
   implicit none
 
   !--------------------------- Parameters for s_curve-----------------------
-  real(kind = x_precision), parameter         :: eps_in = 1.e-7_x_precision  ! Precision required for the dichotomy
+  real(kind = x_precision), parameter         :: eps_in = 1.0e-7_x_precision ! Precision required for the dichotomy
   real(kind = x_precision), parameter         :: Tmin   = 2.5e-2_x_precision
   real(kind = x_precision), parameter         :: Tmax   = 4.49e0_x_precision
   real(kind = x_precision), parameter         :: Smin   = 2.36e1_x_precision
