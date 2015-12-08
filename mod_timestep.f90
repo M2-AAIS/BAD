7d93feb4c8e8a976f663b967a7a824bc48e26288
Antoine Marchal <marchal@iap.fr>
2015-12-08 17:09:16 +0100

    process the timestep function

--- mod_timestep.f90
+++ mod_timestep.f90
@@ -2,6 +2,7 @@
 
 module mod_timestep
   use mod_constants
+  use mod_read_parameters
   implicit none
 
   private
@@ -10,12 +11,14 @@ module mod_timestep
   
 contains
   
-  subroutine timestep (state_in, dt)
+  subroutine timestep (state_in, dt_T, dt_nu)
     implicit none
     
     type (state), intent(in)              :: state_in
-    real(kind = x_precision), intent(out) :: dt    
-    
+    real(kind = x_precision), dimension(n_cell),  intent(out) :: dt_nu, dt_T
+
+    dt_T = state_in%Cv * state_in%T / (state_in%Qplus - state_in%Qminus) / cst_dt
+    dt_nu  = dt_T / (state_in%H / x_state%x)**(2._x_precision)
   end subroutine timestep
   
 end module mod_timestep
