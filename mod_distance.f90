ac90199bb5ccec6a7b9bc8a41d6c91edef4004e6
Antoine Marchal <marchal@iap.fr>
2015-12-08 18:19:04 +0100

    beginning of process to distance

--- mod_distance.f90
+++ mod_distance.f90
@@ -4,11 +4,19 @@ module mod_distance
   use mod_s_curve
   implicit none 
   
-  private
-  
-  
+  private  
+
+  public :: distance
 
   contains 
-    
 
+    subroutine distance(state_in, dist, temp_c, sigma_c)
+      implicit none
+      type(state)                                 :: state_in
+      real(kind = x_precision), dimension(n_cell) :: dist
+      real(kind = x_precision), dimension(n_cell) :: temp_c, sigma_c
+
+      dist = 1. !FIXME
+      
+    end subroutine distance
   end module mod_distance
