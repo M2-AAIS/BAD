2783feb32be21ddf7e12fb2f4263c7391654dfc1
Antoine Marchal <marchal@iap.fr>
2015-12-09 14:59:33 +0100

    Fix compute of dt_nu and dt_T

--- mod_timestep.f90
+++ mod_timestep.f90
@@ -16,15 +16,15 @@ contains
     
     type (state), intent(in)              :: state_in
     real(kind = x_precision), dimension(n_cell),  intent(out) :: dt_nu, dt_T
-!    real(kind = x_precision), dimension(n_cell)               :: diffQ
+    real(kind = x_precision), dimension(n_cell)               :: diffQ
 
-  !  diffQ = abs(state_in%Qplus - state_in%Qminus)
+    diffQ = abs(state_in%Qplus - state_in%Qminus)
 
-  !  where (diffQ <= 0.00001)
-  !     dt_T = state_in%Cv * state_in%T / 0.00001 / cst_dt_T
-  !  elsewhere
-  !     dt_T = state_in%Cv * state_in%T / diffQ / cst_dt_T
-  !  end where
+    where (diffQ <= 0.00001)
+       dt_T = state_in%Cv * state_in%T / 0.00001 / cst_dt_T
+    elsewhere
+       dt_T = state_in%Cv * state_in%T / diffQ / cst_dt_T
+    end where
       ! dt_nu  = dt_T / (state_in%H / x_state%x)**(2._x_precision) / cst_dt_nu
       !  dt_nu = x_state%x**2._x_precision / state_in%nu 
 
@@ -33,7 +33,7 @@ contains
    !      / cst_dt_T
 
    ! dt_T  =  1._x_precision / params%alpha / x_state%Omega / cst_dt_T
-    dt_T  = state_in%Cs / params%alpha / state_in%H / x_state%Omega**2._x_precision
+   ! dt_T  = state_in%Cs / params%alpha / state_in%H / x_state%Omega**2._x_precision
   end subroutine timestep
   
 end module mod_timestep
