ed127fd38e56201e9ecd9ffc9b1ee2f6a93f6ad1
Antoine Marchal <marchal@iap.fr>
2015-12-09 09:18:07 +0100

    new expression of dt_nu/T which not depend of Q+/Q-

--- mod_timestep.f90
+++ mod_timestep.f90
@@ -16,16 +16,22 @@ contains
     
     type (state), intent(in)              :: state_in
     real(kind = x_precision), dimension(n_cell),  intent(out) :: dt_nu, dt_T
-    real(kind = x_precision), dimension(n_cell)               :: diffQ
+!    real(kind = x_precision), dimension(n_cell)               :: diffQ
 
-    diffQ = abs(state_in%Qplus - state_in%Qminus)
+  !  diffQ = abs(state_in%Qplus - state_in%Qminus)
 
-    where (diffQ <= 0.00001)
-       dt_T = state_in%Cv * state_in%T / 0.00001 / cst_dt_T
-    elsewhere
-       dt_T = state_in%Cv * state_in%T / diffQ / cst_dt_T
-    end where
-       dt_nu  = dt_T / (state_in%H / x_state%x)**(2._x_precision) / cst_dt_nu
+  !  where (diffQ <= 0.00001)
+  !     dt_T = state_in%Cv * state_in%T / 0.00001 / cst_dt_T
+  !  elsewhere
+  !     dt_T = state_in%Cv * state_in%T / diffQ / cst_dt_T
+  !  end where
+      ! dt_nu  = dt_T / (state_in%H / x_state%x)**(2._x_precision) / cst_dt_nu
+      !  dt_nu = x_state%x**2._x_precision / state_in%nu 
+
+    dt_nu = x_state%x / abs(state_in%v) / cst_dt_nu
+    dt_T  = (state_in%H / x_state%x)**(2._x_precision) * dt_nu * cst_dt_nu &
+         / cst_dt_T
+ 
   end subroutine timestep
   
 end module mod_timestep
