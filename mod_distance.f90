2d28cd1cdff1f35aacc4299970f6afb46de02f73
Corentin Cadiou <contact@cphyc.me>
2015-12-09 16:38:25 +0100

    distance: use gaussian
    Use one minus the gaussian of the distance divided by the critical
    distance. It reduces the timestep first slowly than in an hardcore
    fashion close to the critical point
    
    It may be better to use a minimal value for tmp so that the prefactor
    doesn't go to 0 but to an epsilon close to 0

--- mod_distance.f90
+++ mod_distance.f90
@@ -13,9 +13,16 @@ module mod_distance
     function pre_factor(state_in, S_c, dist_crit)
       implicit none
       type(state), intent(in)                                 :: state_in
-      real(kind = x_precision)                                :: pre_factor
+      real(kind = x_precision)                                :: pre_factor, tmp
       real(kind = x_precision), dimension(n_cell), intent(in) :: S_c, dist_crit
+      tmp = 1 - exp(-(minval((S_c - state_in%S) / dist_crit))**2)
+
+      if (tmp < 0.95_x_precision) then
+         print*, 'Reducing timestep by', tmp
+      else
+         print*, tmp
+      end if
       
-      pre_factor = min(minval((S_c - state_in%S) / dist_crit), 1._x_precision)      
+      pre_factor = tmp
     end function pre_factor
   end module mod_distance
