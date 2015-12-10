965be354808e6de056ebf12a2eb21d71de61a1f4
Corentin Cadiou <contact@cphyc.me>
2015-12-10 11:54:25 +0100

    constants: reduce output frequency
    Also increase the number of iterations

--- mod_constants.f90
+++ mod_constants.f90
@@ -6,13 +6,13 @@ module mod_constants
   integer, parameter, public :: x_precision  = selected_real_kind(15) ! Numerical precision
   integer, parameter, public :: n_cell       = 256                    ! Number of cells along the disk radius
   ! Specific to main program
-  integer, parameter, public :: n_iterations = 100000                 ! Number of iterations to run
-  integer, parameter, public :: output_freq  = 1                      ! Frequency of the outputs
+  integer, parameter, public :: n_iterations = 10000000               ! Number of iterations to run
+  integer, parameter, public :: output_freq  = 5000                   ! Frequency of the outputs
   real(kind = x_precision), parameter, public :: cst_dt_nu = 100._x_precision
   real(kind = x_precision), parameter, public :: cst_dt_T = 10._x_precision
   ! Specific to s_curve
   integer, parameter, public :: nb_it        = 1000                   ! Number of points for the S curve
-  integer, parameter, public :: max_it       = 1000000000               ! Maximum number of s_curve dichotomy iterations, limit: 1e9
+  integer, parameter, public :: max_it       = 1000000000             ! Maximum number of s_curve dichotomy iterations
 
   ! Physical constants
   real(kind = x_precision), parameter, public :: G       = 6.67408e-8_x_precision ! Gravitationnal cst in cgs
