4c763b0a42d3628995c124c3fac5eb033e731818
Bruno Pagani <bruno.n.pagani@gmail.com>
2015-12-10 19:07:04 +0100

    mod_timestep: Reintroduce “where” structure
    The only relevant changes of 310cfcd was the threshold, the “do”
    structure was only use during debugging of this change.

--- mod_timestep.f90
+++ mod_timestep.f90
@@ -8,12 +8,12 @@ module mod_timestep
   private
 
   public :: timestep
-  
+
 contains
-  
+
   subroutine timestep (state_in, dt_T, dt_nu)
     implicit none
-    
+
     type (state), intent(in)              :: state_in
     real(kind = x_precision), dimension(n_cell),  intent(out) :: dt_nu, dt_T
     real(kind = x_precision), dimension(n_cell)               :: diffQ
@@ -23,15 +23,13 @@ contains
     threshold = 1e10_x_precision
     diffQ = abs(state_in%Qplus - state_in%Qminus)
 
-    do i = 1, n_cell
-       if (diffQ(i) <= threshold) then
-          dt_T(i) = state_in%Cv(i) * state_in%T(i) / threshold / cst_dt_T
-       else
-          dt_T(i) = state_in%Cv(i) * state_in%T(i) / diffQ(i) / cst_dt_T
-       end if
-    end do
-      ! dt_nu  = dt_T / (state_in%H / x_state%x)**(2._x_precision) / cst_dt_nu
-      !  dt_nu = x_state%x**2._x_precision / state_in%nu 
+    where (diffQ <= threshold)
+       dt_T = state_in%Cv * state_in%T / threshold / cst_dt_T
+    elsewhere
+       dt_T = state_in%Cv * state_in%T / diffQ / cst_dt_T
+    end where
+   ! dt_nu  = dt_T / (state_in%H / x_state%x)**(2._x_precision) / cst_dt_nu
+   ! dt_nu = x_state%x**2._x_precision / state_in%nu
 
     dt_nu = x_state%x / abs(state_in%v) / cst_dt_nu
    ! dt_T  = (state_in%H / x_state%x)**(2._x_precision) * dt_nu * cst_dt_nu &
@@ -40,11 +38,6 @@ contains
    ! dt_T  =  1._x_precision / params%alpha / x_state%Omega / cst_dt_T
    ! dt_T  = state_in%Cs / params%alpha / state_in%H / x_state%Omega**2._x_precision
   end subroutine timestep
-  
+
 end module mod_timestep
-
-
-
-
-
 
