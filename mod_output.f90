aaf4148cf8966416e276855af6ceae1cc9dbc1fe
Clément Hottier <clement.hottier@obspm.fr>
2015-12-11 15:54:11 +0100

    add the Qadv in the output

--- mod_output.f90
+++ mod_output.f90
@@ -27,21 +27,21 @@ contains
 
     write(unit, fmt=*) '# ', iteration
     write(unit, fmt=*) '# ', time
-    write(unit, fmt='(20(a16))') 'r', 'Omega*', 'nu', 'v', 'T', 'P_rad',&
+    write(unit, fmt='(21(a16))') 'r', 'Omega*', 'nu', 'v', 'T', 'P_rad',&
          'P_gas', 'beta', 'cs', 'H', 'rho', 'S', 'Sigma', 'tau', 'Fz',&
-         'M_dot', 'Cv', 'Q_+', 'Q_-', 'f'
+         'M_dot', 'Cv', 'Q_+', 'Q_-', 'f', 'Qadv'
 
     state_dim = s
     call dim_adim(1, state_dim)
     do i = 1, n_cell
-       write(unit, fmt='(20(e16.8e2))') r_state%r(i), x_state%Omega(i), &
+       write(unit, fmt='(21(e16.8e2))') r_state%r(i), x_state%Omega(i), &
             state_dim%nu(i), state_dim%v(i), &
             state_dim%T(i), state_dim%Prad(i), state_dim%Pgaz(i), &
             state_dim%beta(i), state_dim%cs(i), &
             state_dim%H(i), state_dim%rho(i), state_dim%S(i), &
             state_dim%S(i) / x_state%x(i), &
             s%tau(i), state_dim%Fz(i), state_dim%Mdot(i), state_dim%Cv(i), &
-            s%Qplus(i), s%Qminus(i), s%Qplus(i) - s%Qminus(i)
+            s%Qplus(i), s%Qminus(i), s%Qplus(i) - s%Qminus(i), s%Qadv(i)
     end do
 
   end subroutine snapshot
