39f7fc65dd2df011c313873157a43ded14ef9143
Corentin Cadiou <contact@cphyc.me>
2015-12-14 21:52:03 +0100

    plt_data: Update boundaries and set log scale on Mdot

--- plot_data.py
+++ plot_data.py
@@ -305,6 +305,8 @@ def init(ic, crit_pts, s_curves, initial_data):
     else:
         ax21.set_ylabel('$\dot{M}\ (\mathrm{g.s^{-1}})$')
     ax21.grid()
+    ax21.set_yscale('log')
+    ax21.set_ylim(1e15, 1e18)
 
     #######################################################################
     #  Bottom right panel
